28c25050945982b79f532cdc0e649deebfae9138
kmy0 <93156259+kmy0@users.noreply.github.com>
2023-03-12 23:08:32 +0100

    Update AHBD.lua

--- reframework/autorun/AHBD.lua
+++ reframework/autorun/AHBD.lua
@@ -1,11 +1,15 @@
 local config = require("AHBD.config")
 local config_menu = require("AHBD.config_menu")
 local data = require("AHBD.data")
+local utilities = require("AHBD.utilities")
 local hitboxes = require("AHBD.hitboxes")
 local hurtboxes = require("AHBD.hurtboxes")
 local drawing = require("AHBD.drawing")
+local misc = require("AHBD.misc")
+
 
 data.init()
+utilities.init()
 config.init()
 config_menu.init()
 drawing.init()
@@ -13,8 +17,7 @@ hitboxes.init()
 hurtboxes.init()
 
 
-sdk.hook(sdk.find_type_definition('snow.VillageState'):get_method('.ctor'), hitboxes.reset)
-sdk.hook(sdk.find_type_definition('snow.VillageState'):get_method('OnDestroy'), hitboxes.reset)
+sdk.hook(sdk.find_type_definition("via.physics.RequestSetCollider"):get_method('getCollidableFromIndex(System.UInt32, System.UInt32, System.UInt32)'), hurtboxes.get_hurtboxes)
 sdk.hook(sdk.find_type_definition('snow.hit.AttackWork'):get_method('initialize(System.Single, System.Single, System.UInt32, System.UInt32, System.Int32, snow.hit.userdata.BaseHitAttackRSData)'), hitboxes.get_attacks)
 
 
@@ -36,20 +39,15 @@ re.on_frame(
             config_menu.draw()
         end
 
-        hitboxes.draw()
-        hurtboxes.draw()
+        if utilities.is_in_quest() then
+            utilities.update_objects()
+            hitboxes.draw()
+            hurtboxes.draw()
+        else
+            hitboxes.reset()
+            hurtboxes.reset()
+        end
     end
 )
 
 re.on_config_save(config.save)
-
---test props
-
---options to display missing stuff?
---check master player stuff
-
---endemic hurtboxes maybe
--- look at imgui demo to improve interface maybe
---flicker on first boot, font related
-
---check EnemyThinkBehavior
